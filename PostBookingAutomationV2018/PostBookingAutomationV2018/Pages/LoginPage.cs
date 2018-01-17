497ee7cd171f3032b63b2c53660db66bd3422181
jahangira <jahangir.ali@easyjet.com>
2018-01-17 14:29:04 +0000

    Added EndTest

---
+++ PostBookingAutomationV2018/PostBookingAutomationV2018/Pages/LoginPage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support;
+
+namespace PostBookingAutomationV2018.Pages
+{
+    public class LoginPage
+    {
+        private IWebDriver Driver { get; }
+        private static readonly By PageSelector = By.CssSelector("div[id=LogOnPanel']");
+
+        public LoginPage(IWebDriver driver)
+        {
+            Driver = driver;
+
+        }
+
+
+    }
+}
--- PostBookingAutomationV2018/PostBookingAutomationV2018/UnitTest1.cs
+++ PostBookingAutomationV2018/PostBookingAutomationV2018/UnitTest1.cs
@@ -1,14 +1,28 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace PostBookingAutomationV2018
 {
     [TestClass]
     public class UnitTest1
     {
+        IWebDriver driver; 
+
         [TestMethod]
-        public void TestMethod1()
+        public void Initialize()
         {
+            driver = new ChromeDriver();
+            //driver.Url = "https://www.119.test/EN/secure/MyEasyJet.mvc/SignIn";
+            driver.Url = "https://www.119.test/TestPages/HomePage.html?lang=EN";
+
+            driver.FindElement(By.CssSelector("div[a href='https://www.119.test/EN/secure/MyEasyJet.mvc/SignIn']"));
+        }
+
+        public void EndTest()
+        {
+            driver.Quit();
         }
     }
 }
