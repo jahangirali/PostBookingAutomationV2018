f07335130a6a45c741d4fdd7d8e0e4fbfa5d2c2d
jahangira <jahangir.ali@easyjet.com>
2018-01-17 15:17:58 +0000

    Added elements and actions for LoginPage

--- PostBookingAutomationV2018/PostBookingAutomationV2018/Pages/LoginPage.cs
+++ PostBookingAutomationV2018/PostBookingAutomationV2018/Pages/LoginPage.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.PageObjects;
 
 namespace PostBookingAutomationV2018.Pages
 {
@@ -16,7 +17,37 @@ namespace PostBookingAutomationV2018.Pages
         public LoginPage(IWebDriver driver)
         {
             Driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
 
+        [FindsBy(How = How.CssSelector, Using = "input[id='emailaddress'")] private IWebElement EmailAddressField;
+        [FindsBy(How = How.CssSelector, Using = "input[id='password']")] private IWebElement PasswordField;
+        [FindsBy(How = How.CssSelector, Using = "input[id='memberbookingreference']")] private IWebElement BookingReferenceField;
+        [FindsBy(How = How.CssSelector, Using = "a[id='register-button']")] private IWebElement RegisterButton;
+        [FindsBy(How = How.CssSelector, Using = "input[id='sign-in-button']")] private IWebElement SignInButton;
+        [FindsBy(How = How.CssSelector, Using = "input[id='memberbookingreference']")] private IWebElement ForgottenPasswordLink;
+
+        public void LoggedIn()
+        {
+            EnterEmailAddress();
+            EnterPassword();
+            ClickSignInButton();
+        }
+
+
+        private void EnterEmailAddress()
+        {
+            EmailAddressField.SendKeys("jahangir.ali@easyjet.com");
+        }
+
+        private void EnterPassword()
+        {
+            PasswordField.SendKeys("password123");
+        }
+
+        private void ClickSignInButton()
+        {
+            SignInButton.Click();
         }
 
 
