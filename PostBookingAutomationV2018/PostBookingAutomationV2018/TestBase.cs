4b0eaab1ef5faceb104b16a276c9e32b7b843c6e
jahangira <jahangir.ali@easyjet.com>
2018-01-17 15:17:08 +0000

    Added NUnit OneTimeSetup & OneTimeTearDown Renamed UnitTest1 to TestBase Added elements and actions for LoginPage

---
+++ PostBookingAutomationV2018/PostBookingAutomationV2018/TestBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace PostBookingAutomationV2018
+{
+    [TestClass]
+    public class TestBase
+    {
+        IWebDriver driver; 
+
+        [OneTimeSetUp]
+        public void Initialize()
+        {
+            driver = new ChromeDriver();
+            driver.Url = "https://www.119.test/EN/secure/MyEasyJet.mvc/SignIn";
+            //driver.Url = "https://www.119.test/TestPages/HomePage.html?lang=EN";
+
+            //driver.FindElement(By.CssSelector("div[a href='https://www.119.test/EN/secure/MyEasyJet.mvc/SignIn']"));
+
+
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
