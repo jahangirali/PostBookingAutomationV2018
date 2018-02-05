35a585a400180cf688829bb6f67882dc5722473f
jahangira <jahangir.ali@easyjet.com>
2018-02-05 17:24:48 +0000

    Added new page UpSellSeatPanelSelectorPage

--- PostBookingAutomationV2018/Selenium/Pages/UpSellSeatPanelSelectorPage.cs
+++ PostBookingAutomationV2018/Selenium/Pages/UpSellSeatPanelSelectorPage.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
 
 namespace Selenium.Pages
 {
     public class UpSellSeatPanelSelectorPage
     {
-
+        private IWebDriver Driver { get; }
+        private static readonly By PageSelector = By.CssSelector("a[id='ContactDetailsLinkBreadcrumb']");
     }
+
+    public ViewBookingsPage(IWebDriver driver)
+    {
+        Driver = driver;
+        PageFactory.InitElements(driver, this);
+    }
+
+    [FindsBy(How = How.Id, Using = "YourBookingsLinkBreadcrumb")] private IWebElement AllBookings;
 }
